2d4494191558ca77e16ef37965f637cc3d64618b
akumar2017 <akumar2017@pingry.org>
2018-12-10 15:39:42 -0500

    Update FirebaseProject.java

--- app/src/main/java/com/example/pdarb/android_project_showcase/FirebaseProject.java
+++ app/src/main/java/com/example/pdarb/android_project_showcase/FirebaseProject.java
@@ -1,16 +1,22 @@
 package com.example.pdarb.android_project_showcase;
 
+import java.util.ArrayList;
+
 public class FirebaseProject {
     public String description;
     public String name;
     public String type;
-
+    public String intro;
+    public String majors;
+    public ArrayList<FirebaseContacts> members;
     public FirebaseProject() {}
 
-    public FirebaseProject(String id, String description, String name, String type) {
+    public FirebaseProject(String id, String description, String name, String type, String intro, String majors) {
         this.description = description;
         this.name = name;
         this.type = type;
+        this.intro = intro;
+        this.majors = majors;
     }
 
     public String getDescription() {
@@ -35,5 +41,21 @@ public class FirebaseProject {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String type) {
+        this.intro = intro;
+    }
+
+    public String getMajors() {
+        return majors;
+    }
+
+    public void setMajors(String majors) {
+        this.majors = majors;
     }
 }
